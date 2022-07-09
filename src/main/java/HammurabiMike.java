65d0835277ad22863c9238d71b545f99341047c2
Tenae <tenae@zipcodersmbp107.myfiosgateway.com>
2022-07-09 18:20:30 -0400

    progress so far 6 methods done

--- src/main/java/HammurabiMike.java
+++ src/main/java/HammurabiMike.java
@@ -5,9 +5,11 @@ public class HammurabiMike {
     Random rand = new Random();  // this is an instance variable
     Scanner scan = new Scanner(System.in);
 
-    int year, population, bushels, landOwned, landSellPrice, starved, newPeople, bushelsPerAcre, bushelsHarvested, bushelsDestroyed;
+    int year, population, bushels, landOwned, landSellPrice, starved, fed, newPeople, bushelsEaten, bushelsPerAcre, bushelsHarvested, bushelsDestroyed;
 
-
+    String FINK = "DUE TO THIS EXTREME MISMANAGEMENT YOU HAVE NOT ONLY\n" +
+            "BEEN IMPEACHED AND THROWN OUT OF OFFICE BUT YOU HAVE\n" +
+            "ALSO BEEN DECLARED PERSONA NON GRATA!!\n";
     public static void main(String[] args) { // required in every Java program
         HammurabiMike m = new HammurabiMike();
         m.playGame();
@@ -16,19 +18,20 @@ public class HammurabiMike {
 
     void playGame() {
         // declare local variables here: grain, population, etc.
-        // statements go after the declations
+        // statements go after the declarations
         setYear(0);
         setPopulation(100);
         setBushels(2800);
         setLandOwned(1000);
         setLandSellPrice(19);
         setStarved(0);
+        setFed(0);
         setNewPeople(5);
         setBushelsPerAcre(19);
         setBushelsHarvested(0);
         setBushelsDestroyed(0);
 
-       //askHowManyAcresToBuy();
+
 
     }
 
@@ -55,6 +58,8 @@ public class HammurabiMike {
     public void setStarved(int starved) {
         this.starved = starved;
     }
+
+    public void setFed(int fed) {this.fed = fed;}
 
     public void setNewPeople(int newPeople) {
         this.newPeople = newPeople;
@@ -95,6 +100,8 @@ public class HammurabiMike {
     public int getStarved() {
         return starved;
     }
+
+    public int setFed() {return fed;}
 
     public int getNewPeople() {
         return newPeople;
@@ -174,24 +181,132 @@ public class HammurabiMike {
         //other methods go here
     }
 
+
+
     public int askHowManyAcresToBuy(int price, int bushels) {
         price = getBushelsPerAcre();
         bushels = getBushels();
-        Scanner input = new Scanner(System.in);
+        //Scanner input = new Scanner(System.in);
         System.out.println("HOW MANY ACRES DO YOU WANT TO BUY?");
-        int acresToBuy = input.nextInt();
+        int acresToBuy = scan.nextInt();
 
+        if (acresToBuy <= 0 ) {epicFail(0);}
 
         if(acresToBuy * price > bushels){
             System.out.println("HAMURABI:  THINK AGAIN. YOU HAVE ONLY\n" +
                     getBushels() + " BUSHELS OF GRAIN. NOW THEN,");
         }
 
+        if (acresToBuy * price < bushels){
 
-        return acresToBuy;
+            landOwned += acresToBuy;
+            bushels -= acresToBuy * price;
+        }
+        return landOwned;
 
 
     }
 
+    public int askHowManyAcresToSell (int acresOwned){
 
+        acresOwned = getLandOwned();
+        System.out.println("HOW MANY ACRES DO YOU WANT TO SELL?");
+        int acresToSell = scan.nextInt();
+
+        if (acresToSell <= 0 ) {epicFail(0);}
+
+        if (acresToSell > getLandOwned()){ System.out.println("HAMURABI:  THINK AGAIN. YOU HAVE ONLY\n" +
+                getBushels() + " BUSHELS OF GRAIN. NOW THEN,");}
+
+        if(acresToSell <= getBushels()){
+
+            landOwned -= acresToSell;
+            bushels += acresToSell * getBushelsPerAcre();
+
+        }
+
+        return landOwned;
+    }
+
+    public int askHowMuchGrainToFeedPeople(int bushels){
+
+        System.out.println("\nHOW MANY BUSHELS DO YOU WISH TO FEED YOUR PEOPLE?  ");
+        int amountOfBushels = scan.nextInt();
+
+        if (amountOfBushels <= 0)
+            epicFail(0);
+        if (amountOfBushels > getBushels()){
+            System.out.println("HAMURABI:  THINK AGAIN. YOU HAVE ONLY\n" +
+                   getBushels() + " BUSHELS OF GRAIN. NOW THEN,");}
+
+       if (amountOfBushels < getBushels()){
+       fed = amountOfBushels / 20;
+        bushels -= amountOfBushels;
+       }
+        return fed;
+
+    }
+
+    public int askHowManyAcresToPlant(int acresOwned, int population, int bushels){
+        System.out.print("\nHOW MANY ACRES DO YOU WISH TO PLANT WITH GRAIN?");
+        int amountOfAcres = scan.nextInt();
+
+        if (amountOfAcres <= 0)
+            epicFail(0);
+        if (amountOfAcres > getLandOwned())
+            System.out.println("HAMURABI:  THINK AGAIN. YOU OWN ONLY " + landOwned + " ACRES. NOW THEN,");
+        if (amountOfAcres / 2 > getBushels())
+            System.out.println("HAMURABI:  THINK AGAIN. YOU HAVE ONLY\n" +
+                    bushels + " BUSHELS OF GRAIN. NOW THEN,");
+        if (amountOfAcres > population * 10)
+            System.out.println("BUT YOU HAVE ONLY" + population + "PEOPLE TO TEND THE FIELDS. NOW THEN,");
+
+        if (amountOfAcres < landOwned || amountOfAcres / 2 < bushels || amountOfAcres < population * 10) {
+            bushelsEaten = bushels - bushelsHarvested ;
+            bushels -= amountOfAcres / 2;
+            int yield = (int) (Math.random() * 5 + 1);
+            bushelsHarvested = amountOfAcres * yield;
+            amountOfAcres = (int) (Math.random() * 5 + 1);
+            if (amountOfAcres % 2 != 1)
+                bushelsEaten = (bushels / amountOfAcres);
+            else
+                bushelsEaten = 0;
+            bushels += (bushelsHarvested - bushelsDestroyed);
+
+
+        }
+
+        return bushels;
+    }
+
+    int plagueDeaths(int population){
+
+        if (rand.nextInt(100) < 15) {
+            return population / 2;
+        }
+        return 0;
+
+    }
+
+    int starvationDeaths(int population, int bushelsFedToPeople){
+        bushelsEaten = bushels - bushelsHarvested;
+        if (population * 20 < bushelsEaten) return 0;
+        return population - (bushelsEaten / 20);
+    }
+
+
+
+
+    private void epicFail(int x) {
+        String reason = "";
+        switch (x) {
+            case 0: reason = "HAMURABI:  I CANNOT DO WHAT YOU WISH.\n" +
+                    "GET YOURSELF ANOTHER STEWARD!!!!!"; break;
+            case 1: reason = "YOU STARVED " + getStarved() + " PEOPLE IN ONE YEAR!!!\n" +
+                    FINK; break;
+
+        }
+        System.out.println(reason);
+        System.exit(0);
+}
 }
